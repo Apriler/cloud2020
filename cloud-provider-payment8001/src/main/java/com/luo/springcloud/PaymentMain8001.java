a53471bbde638cdf7d2af4ab76bb8811e5f425bd
luoaojin <luoaojin@bonc.com.cn>
2020-04-29 17:50:52 +0800

    add

---
+++ cloud-provider-payment8001/src/main/java/com/luo/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.luo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoaojin
+ * @CreateTime 2020-04-29
+ * @Description
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
